1aaa956866827f840e29ae0975fad0508b915783
Snowrabbit <52154610+brumaire18@users.noreply.github.com>
2026-01-27 23:37:40 +0900

    Fix weekly volume flag initialization

--- ui_streamlit.py
+++ ui_streamlit.py
@@ -2671,26 +2671,15 @@ def main():
                         else:
                             selling_climax_ok = False
 
-                    if all(
-                        [
-                            rsi_ok,
-                            macd_ok,
-                            sma_ok,
-                            vol_ok,
-                            rs_ok,
-                            canslim_ok,
-                            new_high_ok,
-                            selling_climax_ok,
-                        ]
-                    ):
-                        weekly_vol_ok = True
-                        weekly_volume = None
                     if apply_weekly_volume_quartile:
                         weekly_volume = weekly_volume_map.get(code_str)
                         if weekly_volume_threshold is None or weekly_volume is None:
                             weekly_vol_ok = False
                         else:
                             weekly_vol_ok = weekly_volume >= weekly_volume_threshold
+                    else:
+                        weekly_vol_ok = True
+                        weekly_volume = None
 
                     if all(
                         [
